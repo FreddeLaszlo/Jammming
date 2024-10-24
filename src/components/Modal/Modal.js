b37aaa490b7d5dda7f87d7319b1d0d4fae9a2887
FreddeLaszlo <86655810+FreddeLaszlo@users.noreply.github.com>
2024-10-24 16:12:09 +0100

    Updated Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -4,45 +4,33 @@
  * by Fred de Laszlo 
  * 15-OCT-2024 
  */
-import React, { useEffect } from 'react';
+import React from 'react';
 import './Modal.css';
 
 /**
  * React compenent that creates HTML to simulate a modal dialog.
- * Set message to non zero length string to show, zewro length to remove.
- * @param {Object} - message (String), handleOnModalOK (callback function)
+ * 
+ * @param {Object} - showModal {Boolean), title (String), message (String), handleOnModalOK (callback function)
  * @returns HTML dialog
  */
 function Modal({ showModal, title, message, handleOnModalOK }) {
 
-    // When the dialog is shown, add keyboard handler.
-    // Return cleanup that removes the keyboard handler.
-    useEffect(() => {
-        if (showModal === true) {
-            console.log("Modal.useEffect: addEventLisdtener");
-            window.addEventListener("keydown", handleKeyDown);
-        }
-        return () => {
-            console.log("Modal.useEffect: removeEventListener");
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    }, [showModal]);
-
-    /**
-     * Handle return, space or esc key pressed
-     * @param {Object} e - the event object
-     */
-    function handleKeyDown(e) {
-        e.preventDefault();
+    // If being shown, add an event listener to detect key presses
+    // for enter, escape and space keys.
+    const handleOnKeyDown = (e) => {
         if (e.code === 'Enter' || e.code === 'Escape' || e.code === 'Space') {
             handleOnModalOK();
         }
-    }
+    };
 
+    // No need for useEffect hook - the event listener is not reliant on                                            
+    // on-screen rendering. 
     if (showModal === false) {
-        // Return false to force unmount and run clean up code (useEffect).
-        return false; 
-    }
+        window.removeEventListener("keydown", handleOnKeyDown);
+        return false;
+    }                                       
+
+    window.addEventListener("keydown", handleOnKeyDown);
 
     return (
         <dialog
